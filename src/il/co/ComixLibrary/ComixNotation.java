70009ddb0c3b7d453e8e8fc82ba77b5c3bc8007a
artmeleshko <artem.meleshko@gmail.com>
2016-08-28 23:31:18 +0300

    The Third commit

--- src/il/co/ComixLibrary/ComixNotation.java
+++ src/il/co/ComixLibrary/ComixNotation.java
@@ -3,7 +3,7 @@ package il.co.ComixLibrary;
 public class ComixNotation {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello World!");
+		System.out.println("Hello World!");
 
 	}
 
