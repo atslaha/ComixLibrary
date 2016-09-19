0e6df8e6f51d45e0b911c51d0f1ab48b95377733
Iren Saturina <sin0988@gmail.com>
2016-09-19 19:51:58 +0300

    Format & Code analysis

--- src/test/resources/DemoTestComixNotation.java
+++ src/test/resources/DemoTestComixNotation.java
@@ -1,28 +1,27 @@
 package test.resources;
 
-import static org.junit.Assert.*;
-
+import il.co.ComixLibrary.ComixNotation;
 import org.junit.Test;
 
-import il.co.ComixLibrary.ComixNotation;
+import static org.junit.Assert.assertTrue;
 
 public class DemoTestComixNotation {
-	
-	@Test
-	public void ShouldReturnTrue(){
-		ComixNotation demoNotation = new ComixNotation();
-		assertTrue(demoNotation.getBool());
-	}
-	
-	@Test
-	public void ShouldReturnTrue1(){
-		ComixNotation demoNotation = new ComixNotation();
-		assertTrue(demoNotation.getBooll());
-	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    @Test
+    public void ShouldReturnTrue() {
+        ComixNotation demoNotation = new ComixNotation();
+        assertTrue(demoNotation.getBool());
+    }
 
-	}
+    @Test
+    public void ShouldReturnTrue1() {
+        ComixNotation demoNotation = new ComixNotation();
+        assertTrue(demoNotation.getBooll());
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
